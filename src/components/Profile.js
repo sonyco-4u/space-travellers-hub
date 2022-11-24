fd06ad13c749755fc7b0194c04c78fabe9c898b6
Odette Gleen <nkargleeny@gmail.com>
2022-11-24 21:21:47 +0100

    files setup

--- src/components/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Profile = () => {
-    return (
-        <p>Profile goes here</p>
-    )
-}
-
-export default Profile;
--- src/components/Rockets.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Rockets = () => {
-    return (
-        <p>This the the rockets page</p>
-    )
-}
-
-export default Rockets;
