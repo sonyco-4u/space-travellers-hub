553f11e369f0d94987f2ee745b2945c2ff63a9c6
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-11-26 04:46:06 -0800

    add testing function

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,30 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import { useSelector } from 'react-redux';
-
-const MyProfile = () => {
-  const rockets = useSelector((state) => state.rocketsReducer);
-  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-
-  return (
-    <main className="profile-main">
-      <div>
-        <h1>My Rockets</h1>
-        <div className="profile-rockets">
-          {
-          reservedRockets.map((rocket) => (
-            <h3 key={rocket.id} className="profile-res-rocket">{rocket.name}</h3>
-          ))
-        }
-        </div>
-      </div>
-      <div>
-        <h1>My Missions</h1>
-      </div>
-    </main>
-  );
-};
-=======
 import MissionProfile from './missionsComponent/missionsProfile';
 import RocketProfile from './rocketsComponent/RocketProfile';
 
@@ -35,6 +9,5 @@ const MyProfile = () => (
     <RocketProfile />
   </main>
 );
->>>>>>> Stashed changes
 
 export default MyProfile;
